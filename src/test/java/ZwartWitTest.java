08cb24fd9399c45493c684864e5563e389a9db7b
AccursedOath <atlasflamezz@outlook.com>
2021-06-06 16:38:35 +0200

    Kleine aanpassingen aan tests

--- src/test/java/ZwartWitTest.java
+++ src/test/java/ZwartWitTest.java
@@ -8,20 +8,20 @@ class ZwartWitTest {
     @Test
     public void prijs(){
         Gebruiker d = new Gebruiker("D","D","D");
-        ZwartWit x = new ZwartWit(d,10,1); //return super.getHoeveelheid() * 10;
+        ZwartWit x = ZwartWit.maakZwartWit(d,10,1); //return super.getHoeveelheid() * 10;
         assertEquals(x.berekenPrijs(),100);
     }
     //1postnl hv < 100 return 500     else return hv x 5    2DHL: (hoeveelheid * 10) + 1000;
     @Test
     public void PostNLprijs(){
         Gebruiker d = new Gebruiker("D","D","D");
-        ZwartWit x = new ZwartWit(d,10,1); //return super.getHoeveelheid() * 10;
+        ZwartWit x = ZwartWit.maakZwartWit(d,10,1); //return super.getHoeveelheid() * 10;
         assertEquals(x.berekenPrijs() + x.berekenLocatiePrijs(), 100 + 500);
     }
 
     public void DHLprijs(){
         Gebruiker d = new Gebruiker("D","D","D");
-        ZwartWit x = new ZwartWit(d,10,2); //return super.getHoeveelheid() * 10;
+        ZwartWit x = ZwartWit.maakZwartWit(d,10,2); //return super.getHoeveelheid() * 10;
         assertEquals(x.berekenPrijs() + x.berekenLocatiePrijs(), 100 + 1100);
     }
 }
